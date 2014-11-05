df525a3612fd82c2ff0bf74b0179cc18efaf702f
k4lls <materre@gmail.com>
2014-11-05 18:08:02 +0100

    v3

--- m_get_design.m
+++ m_get_design.m
@@ -1,4 +1,4 @@
-function [] = m_get_design( main_file,handles,display_msg )
+function [handles] = m_get_design( main_file,handles,display_msg )
 % DEFINES VALUE FOR THE ASSOCIATED KEYWORDS
 % INPUT  : file_content (cell array)
 % OUTPUT : lan (structure)
@@ -8,13 +8,7 @@ function [] = m_get_design( main_file,handles,display_msg )
 
     % FIND KEY WORDS AND DEFINE OBJECT !
     max_rows=size(handles.CHANNEL.ch_info,2);
-        tbl_content=cell(max_rows,4);
-    % initialized tbl_content
-    for i=1:max_rows
-        for j=1:4
-           tbl_content{i,j}='';
-        end
-    end
+    tbl_content=cell(max_rows,10);
     
      % FIND CHANNEL #
      ch_num=zeros(max_rows,1);
@@ -34,23 +28,87 @@ file_content=l_get_file_content( main_file,handles,display_msg );
     else
         nb_of_file_line=size(file_content,1);
     end
-    
+
     % CREATE TABLE TO DISPLAY
     for i=1:nb_of_file_line
         key_sch=l_search_key(['$geometryline' num2str(i)],file_content,main_file);
-        geo=textscan(key_sch,'%s%s%s%s','delimiter','|');
-        for j=1:4
+        if handles.main.type==1 % TX
+            geo=textscan(key_sch,'%s%s%s%s%s%s%s%s%s','delimiter','|');
+        elseif handles.main.type==0 % RX
+            geo=textscan(key_sch,'%s%s%s%s%s%s%s','delimiter','|');
+        end
+        for j=1:7
             if isempty(geo{1,j})
                 geo{1,j}{1,1}='';
             end
         end       
         tbl_content{i,1}=geo{1,1}{1,1};
-        tbl_content{i,2}=geo{1,2}{1,1};
-        tbl_content{i,3}=geo{1,3}{1,1};
-        tbl_content{i,4}=geo{1,4}{1,1};
-
+        if ~strcmp(geo{1,1}{1,1},'Off') && ~strcmp(geo{1,1}{1,1},'')
+        tbl_content{i,2}=str2double(geo{1,2}{1,1});
+        tbl_content{i,3}=str2double(geo{1,3}{1,1});
+        tbl_content{i,4}=str2double(geo{1,4}{1,1});
+        tbl_content{i,5}=str2double(geo{1,5}{1,1});
+        tbl_content{i,6}=str2double(geo{1,6}{1,1});
+        tbl_content{i,7}=str2double(geo{1,7}{1,1});
+        end
+        handles.Ant.num{i}=str2double(geo{1,6}{1,1});
+        handles.Ant.azm{i}=str2double(geo{1,7}{1,1});
+        if handles.main.type==1
+        handles.TX.type{i}=geo{1,8}{1,1};
+        handles.TX.sn{i}=geo{1,9}{1,1};
+        end
+    end
+    
+    
+    key_Line=l_search_key('$geometryXstn',file_content,main_file);
+    if strcmp(key_Line,'$geometryXstn')
+        set(handles.Xstn_box,'String','')    
+    else
+        set(handles.Xstn_box,'String',key_Line)
     end
 
+    key_Line=l_search_key('$geometryYstn',file_content,main_file);
+    if strcmp(key_Line,'$geometryYstn')
+        set(handles.Ystn_box,'String','')    
+    else
+        set(handles.Ystn_box,'String',key_Line)
+    end
+    
+    key_Line=l_search_key('$geometryLineName',file_content,main_file);
+    if strcmp(key_Line,'$geometryLineName')
+        set(handles.line_box,'String','')    
+    else
+        set(handles.line_box,'String',key_Line)
+    end
+    
+    key_Line=l_search_key('$geometryLineAzimuth',file_content,main_file);
+    if strcmp(key_Line,'$geometryLineAzimuth')
+        set(handles.SX_azimut_box,'String','')    
+    else
+        set(handles.SX_azimut_box,'String',key_Line)
+    end
+
+    key_Line=l_search_key('$geometryAspace',file_content,main_file);
+    if strcmp(key_Line,'$geometryAspace')
+        set(handles.a_space_box,'String','')    
+    else
+        set(handles.a_space_box,'String',key_Line)
+    end
+    
+    key_Line=l_search_key('$geometrySspace',file_content,main_file);
+    if strcmp(key_Line,'$geometrySspace')
+        set(handles.s_space_box,'String','')    
+    else
+        set(handles.s_space_box,'String',key_Line)
+    end
+    
+    key_Line=l_search_key('$geometryZpositive',file_content,main_file);
+    if strcmp(key_Line,'$geometryZpositive')
+        set(handles.z_positive,'Value',1)    
+    else
+        set(handles.z_positive,'Value',str2double(key_Line))
+    end
+    
 end
     
     set(handles.geometry_table,'data',tbl_content,'RowName',ch_num)
