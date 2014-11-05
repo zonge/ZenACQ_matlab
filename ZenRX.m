fd5db4ef13d8abc9f5a24af5d04d2bdaa0ea23d4
k4lls <materre@gmail.com>
2014-11-05 18:06:25 +0100

    v3

--- ZenRX.m
+++ ZenRX.m
@@ -1,10 +1,10 @@
 function varargout = ZenRX(varargin)
 % ZENRX MATLAB code for zenrx.fig
-%      ZENRX set the receiver information and commit the action
+%      ZENRX set the receiver and transmiter information
+% and commit/transmit the action
 
-% Last Modified by GUIDE v2.5 09-Oct-2014 06:10:50
+% Last Modified by GUIDE v2.5 05-Nov-2014 12:24:14
 
-% Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
@@ -43,8 +43,12 @@ guidata(hObject, handles);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function stn_box_Callback(~, ~, handles)
-set(handles.error_msg,'String','');
+% BOX
+function job_name_box_Callback(~, ~, ~)
+function job_number_box_Callback(~, ~, ~)
+function job_by_box_Callback(~, ~, ~)
+function job_for_box_Callback(~, ~, ~)
+function job_operator_box_Callback(~, ~, ~)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -59,15 +63,246 @@ set(handles.error_msg,'String','');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% USER PARAMETERS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function Xstn_box_Callback(hObject, ~, handles)
+
+set(handles.error_msg,'Visible','off');
+Zen_stn=get(hObject,'String');
+
+if isnan(str2double(Zen_stn))==1 || str2double(Zen_stn) ~= floor(str2double(Zen_stn))
+    beep
+    set(handles.error_msg,'Visible','on','String',handles.language.input_err1)
+    set(handles.Xstn_box,'BackgroundColor','red')
+    pause(0.1)
+    set(handles.Xstn_box,'BackgroundColor',[1.0 0.949 0.867])
+    pause(0.1)
+    set(handles.Xstn_box,'BackgroundColor','red')
+    pause(0.1)
+    set(handles.Xstn_box,'BackgroundColor',[1.0 0.949 0.867])  
+    return;       
+end
+
+ geometry_tbl=get(handles.geometry_table,'data');
+
+ 
+ for i=1:size(geometry_tbl,1)
+      if isempty(geometry_tbl{i,1})
+         geometry_tbl{i,1}='Off';
+      end
+ end
+
+set(handles.geometry_table,'data',geometry_tbl)
+set(handles.geometry_table,'Enable','on')
+Ystn_box_str=get(handles.Ystn_box,'String');
+if isempty(Ystn_box_str)
+    set(handles.Ystn_box,'String','0')
+end
+set(handles.geometry_table,'Enable','on')
+set(handles.line_box,'Enable','on')
+set(handles.line_str,'Enable','on')
+set(handles.SX_azimut_box,'Enable','on')
+set(handles.SX_azimut_str,'Enable','on')
+set(handles.a_space_str,'Enable','on')
+set(handles.a_space_box,'Enable','on')
+set(handles.s_space_str,'Enable','on')
+set(handles.s_space_box,'Enable','on')
+set(handles.save_push,'Enable','on')
+
+if handles.main.type==0 % RX
+set(handles.check_setup,'Enable','on')
+set(handles.z_positive_str,'Enable','on')
+set(handles.z_positive,'Enable','on')
+end
+
+function Ystn_box_Callback(hObject, ~, handles)
+    
+set(handles.error_msg,'Visible','off');
+Zen_stn=get(hObject,'String');
+
+if isnan(str2double(Zen_stn))==1 || str2double(Zen_stn) ~= floor(str2double(Zen_stn))
+    beep
+    set(handles.error_msg,'Visible','on','String',handles.language.input_err1)
+    set(handles.Xstn_box,'BackgroundColor','red')
+    pause(0.1)
+    set(handles.Xstn_box,'BackgroundColor',[1.0 0.949 0.867])
+    pause(0.1)
+    set(handles.Xstn_box,'BackgroundColor','red')
+    pause(0.1)
+    set(handles.Xstn_box,'BackgroundColor',[1.0 0.949 0.867])  
+    return;       
+end 
+function line_box_Callback(hObject, ~, handles)
+
+Line_num=get(hObject,'String');
+if ~isempty(Line_num)
+if size(Line_num,2)>16  % CHECK LINE.NUMBER <17
+    beep
+    set(handles.error_msg,'Visible','on','String',handles.language.input_err2)
+    set(hObject,'BackgroundColor','red')
+    pause(0.1)
+    set(hObject,'BackgroundColor','white')
+    pause(0.1)
+    set(hObject,'BackgroundColor','red')
+    pause(0.2)
+    set(hObject,'BackgroundColor','white')  
+    set(hObject,'String',[]);
+    return;       
+end
+end
+function SX_azimut_box_Callback(hObject, ~, handles)
+
+    
+    SX_azimuth=str2double(get(hObject,'String'));
+    if SX_azimuth<0 || SX_azimuth>360 || isnan(SX_azimuth)  % CHECK FOR VALUE OUT OF RANGE
+        beep
+        set(handles.error_msg,'Visible','on','String',handles.language.input_err3)
+        set(handles.survey_panel,'BackgroundColor','red')
+        pause(0.1)
+        set(handles.survey_panel,'BackgroundColor',[0.941 0.941 0.941])
+        pause(0.1)
+        set(handles.survey_panel,'BackgroundColor','red')
+        pause(0.1)
+        set(handles.survey_panel,'BackgroundColor',[0.941 0.941 0.941])
+        set(hObject,'String',0);
+        return;
+    end
+    
+
+    UTM_toggle=get(handles.utm_checkbox,'Value'); %returns toggle state of utm_checkbox
+    DATA=get(handles.geometry_table,'Data');
+    A_space=str2double(get(handles.a_space_box,'String')); 
+    S_space=str2double(get(handles.s_space_box,'String')); 
+    z_positive=get(handles.z_positive,'Value');
+    ZenUTM.X=get(handles.utm_zone_str,'Value');
+    ZenUTM.Y=get(handles.altitude_str,'Value');
+
+handles=update_table( handles,DATA,UTM_toggle,A_space,S_space,SX_azimuth,z_positive,ZenUTM );
+
+% SAVE HANDLES and HObjects
+guidata(hObject, handles);
+function a_space_box_Callback(hObject, ~, handles)
+
+    
+    set(handles.error_msg,'Visible','off')
+    A_space=str2double(get(hObject,'String')); % CHECK FOR VALUE OUT OF RANGE
+    if isnan(A_space)
+        beep
+        set(handles.error_msg,'Visible','on','String',handles.language.input_err4)
+        set(hObject,'BackgroundColor','red')
+        pause(0.1)
+        set(hObject,'BackgroundColor',[0.941 0.941 0.941])
+        pause(0.1)
+        set(hObject,'BackgroundColor','red')
+        pause(0.1)
+        set(hObject,'BackgroundColor',[0.941 0.941 0.941])
+    end
+    
+    
+UTM_toggle=get(handles.utm_checkbox,'Value'); %returns toggle state of utm_checkbox
+DATA=get(handles.geometry_table,'Data');
+S_space=str2double(get(handles.s_space_box,'String'));   
+SX_azimuth=str2double(get(handles.SX_azimut_box,'String'));
+z_positive=get(handles.z_positive,'Value');
+ZenUTM.X=get(handles.utm_zone_str,'Value');
+ZenUTM.Y=get(handles.altitude_str,'Value');
+
+handles=update_table( handles,DATA,UTM_toggle,A_space,S_space,SX_azimuth,z_positive,ZenUTM );
+
+% SAVE HANDLES and HObjects
+guidata(hObject, handles);
+function s_space_box_Callback(hObject, ~, handles)
+
+
+    % CHECK FOR VALUE OUT OF RANGE
+    set(handles.error_msg,'Visible','off')
+    S_space=str2double(get(hObject,'String'));
+    if isnan(S_space)
+        beep
+        set(handles.error_msg,'Visible','on','String','Must be an number !')
+        set(hObject,'BackgroundColor','red')
+        pause(0.1)
+        set(hObject,'BackgroundColor',[0.941 0.941 0.941])
+        pause(0.1)
+        set(hObject,'BackgroundColor','red')
+        pause(0.1)
+        set(hObject,'BackgroundColor',[0.941 0.941 0.941])
+    end
+    
+
+UTM_toggle=get(handles.utm_checkbox,'Value'); %returns toggle state of utm_checkbox
+DATA=get(handles.geometry_table,'Data');
+A_space=str2double(get(handles.a_space_box,'String'));   
+SX_azimuth=str2double(get(handles.SX_azimut_box,'String'));
+z_positive=get(handles.z_positive,'Value');
+ZenUTM.X=get(handles.utm_zone_str,'Value');
+ZenUTM.Y=get(handles.altitude_str,'Value');
+
+handles=update_table( handles,DATA,UTM_toggle,A_space,S_space,SX_azimuth,z_positive,ZenUTM );
+
+% SAVE HANDLES and HObjects
+guidata(hObject, handles);
+function z_positive_Callback(hObject, ~, handles)
+
+z_positive=get(hObject,'Value');
+
+UTM_toggle=get(handles.utm_checkbox,'Value'); %returns toggle state of utm_checkbox
+DATA=get(handles.geometry_table,'Data');
+A_space=str2double(get(handles.a_space_box,'String')); 
+S_space=str2double(get(handles.s_space_box,'String')); 
+SX_azimuth=str2double(get(handles.SX_azimut_box,'String'));
+ZenUTM.X=get(handles.utm_zone_str,'Value');
+ZenUTM.Y=get(handles.altitude_str,'Value');
+
+handles=update_table( handles,DATA,UTM_toggle,A_space,S_space,SX_azimuth,z_positive,ZenUTM );
+
+% SAVE HANDLES and HObjects
+guidata(hObject, handles);
+function utm_checkbox_Callback(hObject, ~, handles)
+
+UTM_toggle=get(hObject,'Value'); %returns toggle state of utm_checkbox
+
+DATA=get(handles.geometry_table,'Data');
+A_space=str2double(get(handles.a_space_box,'String'));
+S_space=str2double(get(handles.s_space_box,'String'));   
+SX_azimuth=str2double(get(handles.SX_azimut_box,'String'));
+z_positive=get(handles.z_positive,'Value');
+ZenUTM.X=get(handles.utm_zone_str,'Value');
+ZenUTM.Y=get(handles.altitude_str,'Value');
+
+handles=update_table( handles,DATA,UTM_toggle,A_space,S_space,SX_azimuth,z_positive,ZenUTM );
+
+% SAVE HANDLES and HObjects
+guidata(hObject, handles);
+
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 % SURVEY SELECTION ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 function design_popup_Callback(hObject, ~, handles)
 
 contents = cellstr(get(hObject,'String'));
-main_file=['design' filesep contents{get(hObject,'Value')} '.geo'];
-
+if handles.main.type==0 % RX
+main_file=['design' filesep contents{get(hObject,'Value')} '.RXgeo'];
+elseif handles.main.type==1 % TX
+main_file=['design' filesep contents{get(hObject,'Value')} '.TXgeo'];
+end
 % UPDATE TABLE
-m_get_design( main_file,handles,false )
+handles=m_get_design( main_file,handles,false );
+
+UTM_toggle=get(handles.utm_checkbox,'Value'); %returns toggle state of utm_checkbox
+DATA=get(handles.geometry_table,'Data');
+A_space=str2double(get(handles.a_space_box,'String'));
+S_space=str2double(get(handles.s_space_box,'String'));  
+SX_azimuth=str2double(get(handles.SX_azimut_box,'String'));
+z_positive=get(handles.z_positive,'Value');
+ZenUTM.X=get(handles.utm_zone_str,'Value');
+ZenUTM.Y=get(handles.altitude_str,'Value');
+
+handles=update_table( handles,DATA,UTM_toggle,A_space,S_space,SX_azimuth,z_positive,ZenUTM );
+
+% SAVE HANDLES and HObjects
+guidata(hObject, handles);
 
 
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -76,18 +311,35 @@ m_get_design( main_file,handles,false )
 function save_push_Callback(hObject, ~, handles)
 
 geometry_tbl=get(handles.geometry_table,'data');
+survey.Xstn_box=get(handles.Xstn_box,'String');
+survey.Ystn_box=get(handles.Ystn_box,'String');
+survey.line_name=get(handles.line_box,'String');
+survey.SX_azimut_box=get(handles.SX_azimut_box,'String');
+survey.a_space_box=get(handles.a_space_box,'String');
+survey.s_space_box=get(handles.s_space_box,'String');
+survey.z_positive=get(handles.z_positive,'Value');
+
 
 % SAVE SURVEY
-handles = ZenRX_save_survey( handles,geometry_tbl );
+handles = ZenRX_save_survey( handles,geometry_tbl,survey );
 
 % SAVE HANDLES and HObjects
 guidata(hObject, handles);
 
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% SURVEY TABLE AUTO-FILL ++++++++++++++++++++++++++++++++++++++++++++++++++
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function geometry_table_CellEditCallback(hObject, eventdata, handles)
+ 
+handles = ZenRX_survey_autofill( handles,eventdata );
+
+% SAVE HANDLES and HObjects
+guidata(hObject, handles);
 
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-% CRES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% CHECK SETUP +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-function get_cres_Callback(~, ~, handles)
+function check_setup_Callback(hObject, ~, handles)
 
 % STOP TIMER
 stop(handles.timer_RX);
@@ -97,6 +349,9 @@ handles = ZenRX_Cres( handles );
 
 % START TIMER
 start(handles.timer_RX);
+
+% SAVE HANDLES and HObjects
+guidata(hObject, handles);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -129,7 +384,15 @@ function edit_push_Callback(hObject, ~, handles)
 contents = cellstr(get(handles.schedule_popup,'String'));
 file_name=contents{get(handles.schedule_popup,'Value')};
 
-edit_file.file=['schedule' filesep file_name '.sch'];
+survey_type=str2double(handles.setting.ZenACQ_mode);
+if handles.main.type==0 && survey_type==1
+    EXT='MTsch';
+elseif handles.main.type==0 && survey_type==2
+    EXT='IPsch';
+elseif handles.main.type==1 && survey_type==2
+    EXT='TXsch';
+end
+edit_file.file=['schedule' filesep file_name '.' EXT];
 edit_file.status=true;
 
 % edit schedule and update GUI
@@ -145,7 +408,15 @@ guidata(hObject, handles);
 function schedule_popup_Callback(hObject, ~, handles)
 
 contents = cellstr(get(hObject,'String'));
-main_file=['schedule' filesep contents{get(hObject,'Value')} '.sch'];
+survey_type=str2double(handles.setting.ZenACQ_mode);
+if handles.main.type==0 && survey_type==1
+    EXT='MTsch';
+elseif handles.main.type==0 && survey_type==2
+    EXT='IPsch';
+elseif handles.main.type==1 && survey_type==2
+    EXT='TXsch';
+end
+main_file=['schedule' filesep contents{get(hObject,'Value')} '.' EXT];
 
 % CREATE SCH OBJ
 handles.SCHEDULE = m_get_sch_obj( main_file,handles,true );
@@ -169,13 +440,13 @@ com.mathworks.mwswing.MJUtilities.initJIDE;
 % Display a DateChooserPanel
 jPanel = com.jidesoft.combobox.DateChooserPanel;
 jPanel.setShowWeekNumbers(false);
-[hPanel,hContainer] = javacomponent(jPanel,[10,250,730,350],gcf);
+[hPanel,hContainer] = javacomponent(jPanel,[10,18,650,370],gcf);
 set(hPanel,'ShowTodayButton',false);
 set(hPanel,'ShowNoneButton',false);
 handles.jPanel=jPanel;
 hModel = handle(hPanel.getSelectionModel, 'CallbackProperties');
 
-set(hPanel, 'ItemStateChangedCallback', {@myCallbackFunction,handles,jPanel,hModel,hContainer});
+set(hPanel, 'ItemStateChangedCallback', {@myCallbackFunction,hObject,handles,jPanel,hModel,hContainer});
 
 % SAVE HANDLES and HObjects
 guidata(hObject, handles);
@@ -208,7 +479,7 @@ guidata(hObject, handles);
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 % CALANDER CALLBACK +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-function myCallbackFunction(~, ~,handles,jPanel,hModel,hContainer)
+function myCallbackFunction(~, ~,hObject,handles,jPanel,hModel,hContainer)
 
 selectedDate = hModel.getSelectedDate();
 dayNumber  = get(selectedDate, 'Date');
@@ -221,7 +492,10 @@ jPanel.setVisible(false);
 delete(hContainer)
 
 % UPDATE GUI
-l_Rx_update_time( handles );
+handles=l_Rx_update_time( handles );
+
+% SAVE HANDLES and HObjects
+guidata(hObject, handles);
 
 
 
@@ -240,6 +514,18 @@ function set_up_Callback(hObject, ~, handles)
 
 % STOP TIMER
 stop(handles.timer_RX);
+
+UTM_toggle=0;
+set(handles.utm_checkbox,'Value',UTM_toggle);
+DATA=get(handles.geometry_table,'Data');
+A_space=str2double(get(handles.a_space_box,'String'));
+S_space=str2double(get(handles.s_space_box,'String'));  
+SX_azimuth=str2double(get(handles.SX_azimut_box,'String'));
+z_positive=get(handles.z_positive,'Value');
+ZenUTM.X=get(handles.utm_zone_str,'Value');
+ZenUTM.Y=get(handles.altitude_str,'Value');
+
+handles=update_table( handles,DATA,UTM_toggle,A_space,S_space,SX_azimuth,z_positive,ZenUTM );
 
 % COMMIT ACTION
 handles = ZenRX_commit( handles );
