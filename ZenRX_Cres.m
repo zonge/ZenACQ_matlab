1382dcfbdcbc8c47af924657f4e8f8cc2d9a36e1
k4lls <materre@gmail.com>
2014-11-05 18:06:13 +0100

    v3

--- ZenRX_Cres.m
+++ ZenRX_Cres.m
@@ -10,15 +10,24 @@ progress = waitbar(0,'Retrieving CRES ...','Name','Contact Resistance' ...
     handles.main.GUI.width_bar handles.main.GUI.height_bar]);
 
 tbl_content=get(handles.geometry_table,'data');
+ch_num=get(handles.geometry_table,'RowName');
+DATA=get(handles.geometry_table,'Data');
 
 for i=1:size(handles.CHANNEL.ch_serial,2)   
-     QuickSendReceive(handles.CHANNEL.ch_serial{1,i},'CONTACTRESISTANCE',10,'atten mask:',0); 
+     index=strfind(ch_num',num2str(handles.CHANNEL.ch_info{1,i}.ChNb));
+     QuickSendReceive(handles.CHANNEL.ch_serial{1,index},'CONTACTRESISTANCE',10,'atten mask:',0); 
 end
 
 for i=1:size(handles.CHANNEL.ch_serial,2) 
-     Contact_res_content = waitln(handles.CHANNEL.ch_serial{1,i},'Brd339>','Read Contact Resistance',10);
-     Contact_RES_str=GetString(Contact_res_content,'CRES(ROut):','ContactResistanceCommand:Complete.'); 
-     tbl_content{i,4}=num2str(round(str2double(Contact_RES_str)));
+     index=strfind(ch_num',num2str(handles.CHANNEL.ch_info{1,i}.ChNb));
+     Contact_res_content = waitln(handles.CHANNEL.ch_serial{1,index},'Brd339>','Read Contact Resistance',10);
+     Contact_RES_str=GetString(Contact_res_content,'CRES(ROut):','ContactResistanceCommand:Complete.');
+     if ~strcmp(DATA{i,1},'Off')
+        tbl_content{i,10}=round(str2double(Contact_RES_str));
+     else
+        tbl_content{i,10}=[];
+     end
+     handles.CRES_values{i}=tbl_content{i,10};
       
      pourcentage=[sprintf('%0.2f',(i/size(handles.CHANNEL.ch_serial,2))*100) ' %'];
      waitbar(i/size(handles.CHANNEL.ch_serial,2),progress,sprintf('%s',pourcentage));      
