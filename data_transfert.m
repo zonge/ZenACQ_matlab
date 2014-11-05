3df19b3b239fda2f8e4b510862b2adcdc78bd42a
k4lls <materre@gmail.com>
2014-11-05 18:08:51 +0100

    v3

--- data_transfert.m
+++ data_transfert.m
@@ -1,4 +1,4 @@
-function [ handles ] = data_transfert( handles,EXTENSION )
+function [ handles,dir_path ] = data_transfert( handles,EXTENSION )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,22 +6,24 @@ function [ handles ] = data_transfert( handles,EXTENSION )
         dir_path=copy_files(handles.main.GUI.left_bar,handles.main.GUI.bottom_bar, ...
             handles.main.GUI.width_bar,handles.main.GUI.height_bar,handles.path_output,EXTENSION);
         if strcmp(dir_path,'empty')
-            warndlg('No z3D files found','Copy Z3Ds')
+            warndlg(handles.language.data_transfer_msg2,handles.language.progress_title5)
         else
             
-            choice2 = questdlg('Do you want to erase SD cards ?', ...
-                'Delete Z3Ds', ...
-                'YES','NO','NO');
+            choice2 = questdlg(handles.language.data_transfer_msg3, ...
+                handles.language.progress_title6, ...
+                handles.language.yes,handles.language.no,handles.language.no);
             
             switch choice2
                 case 'YES'
                     delete_files(handles.main.GUI.left_bar,handles.main.GUI.bottom_bar, ...
-                        handles.main.GUI.width_bar,handles.main.GUI.height_bar)  
+                        handles.main.GUI.width_bar,handles.main.GUI.height_bar,EXTENSION)  
                 case 'NO'
                     return;
             end
             
         end
+        delete_files(handles.main.GUI.left_bar,handles.main.GUI.bottom_bar, ...
+        handles.main.GUI.width_bar,handles.main.GUI.height_bar,'*.CSV')  
 
 end
 
