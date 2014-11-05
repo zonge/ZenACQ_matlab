69c9aded07293454c86d12f041cb20892d89a71c
k4lls <materre@gmail.com>
2014-11-05 18:07:07 +0100

    v3

--- ZenACQ_calibration.m
+++ ZenACQ_calibration.m
@@ -14,7 +14,7 @@ if status==0;handles.CH1_index=CH1_index;else return;end
 
 %% SET MUX | CLEAR METADATA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-progress = waitbar(0,'SET CAL / CLEAR METADATA','Name','SETUP' ...
+progress = waitbar(0,handles.language.progress_str1,'Name',handles.language.progress_title4 ...
 ,'Position',[handles.main.GUI.left_bar handles.main.GUI.bottom_bar ...
 handles.main.GUI.width_bar handles.main.GUI.height_bar]);   
 
@@ -27,7 +27,7 @@ close(progress)
 
 %% WAIT FOR SYNC %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    progress = waitbar(0,'SYNC','Name','SYNC' ...
+    progress = waitbar(0,handles.language.progress_str7,'Name',handles.language.progress_title4 ...
     ,'Position',[handles.main.GUI.left_bar handles.main.GUI.bottom_bar ...
     handles.main.GUI.width_bar handles.main.GUI.height_bar]);   
 
@@ -42,7 +42,7 @@ while sync<1
         if status==2
             error_val=error_val+1;
             if error_val>3    
-                errordlg('The Zen is not responding [ sync ]. The syncing has stopped. You should relunch the program or/and restart your Zen, othersiwe you are likely to encounter issues.','Lost Zen communication.');
+                errordlg(handles.language.sync_err1,handles.language.progress_title4);
                 close(progress)
                 loop_status=false;
                 break;
@@ -53,7 +53,7 @@ while sync<1
     end
 
     if isempty(strfind(data,'N'));sync=1; end
-    waitbar(1,progress,sprintf('%s',['Waiting for channels to sync : ' data ' | Sats : ' numsat]));
+    waitbar(1,progress,sprintf('%s',[handles.language.sync_err2 ' : ' data ' | ' handles.language.sync_err3 ' : ' numsat]));
     pause(3)
 end
 
@@ -72,31 +72,36 @@ time_GPS_dec=datenum(timeGPS,'yyyy-mm-dd,HH:MM:SS');
 time_GPS_dec=time_GPS_dec+define_process_time/86400;
 start_time=datestr(time_GPS_dec,'yyyy-mm-dd,HH:MM:SS');
 
-%% COLLECT CALLIBRATION 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-timerVal = tic;
-FREQ=[1.5625E-2,3.1250E-2,6.2500E-2,0.125,0.25,0.5,1,2,4,8,16,32,64];
-DUTY=100;
-SR=[0,0,0,0,0,0,0,0,0,0,0,0,0];
-gain=[0,0,0,0,0,0,0,0,0,0,0,0,0];
-cycle=[2,2,2,4,4,8,8,16,32,64,128,256,512];
-run_for=ceil((1./FREQ).*cycle+(1./FREQ)+6);
-time_between_files=3;
-letters='_LCAL';
+%% SET CALIBRATOR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % CAL voltage
 QuickSendReceive(handles.CHANNEL.ch_serial{CH1_index},'calvoltage 0.5',10,'Voltageto:',';');
     
 % CAL channel
 QuickSendReceive(handles.CHANNEL.ch_serial{CH1_index},'calchannels 0xff',10,'maskiscurrentlysetto:0x','.');
-    
+
+
+%% COLLECT CALLIBRATION 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+timerVal = tic;
+%FREQ=[1.5625E-2,3.1250E-2,6.2500E-2,0.125,0.25,0.5,1,2,4,8,16,32,64];
+FREQ=[0.5,1,2,4,8];
+DUTY=100;
+SR=[0,0,0,0,0];
+gain=[0,0,0,0,0];
+%cycle=[2,2,2,4,4,8,8,16,32,64,128,256,512];
+cycle=[4,8,8,16,32];
+run_for=ceil((1./FREQ).*cycle+(1./FREQ)+6);
+time_between_files=3;
+letters='_LCAL';
+
 % RUN CALIBRATION ACQUISTION
-[run_in]=l_run_schedule(start_time,false,1,letters,handles,FREQ,DUTY,SR,gain,run_for,time_between_files,0);
+[run_in]=l_run_schedule(start_time,false,letters,handles,FREQ,DUTY,SR,gain,run_for,time_between_files,0);
 run_in=run_in+define_process_time-ceil(toc(timerVal))+2;
 
 
 %% WAIT FOR END OF ACQUISITION
 
-    progress = waitbar(0,'CALIBRATION','Name','CALIBRATION' ...
+    progress = waitbar(0,handles.language.progress_title4,'Name',handles.language.progress_title4 ...
     ,'Position',[handles.main.GUI.left_bar handles.main.GUI.bottom_bar ...
     handles.main.GUI.width_bar handles.main.GUI.height_bar]);   
     set(progress, 'WindowStyle','modal', 'CloseRequestFcn','');
@@ -105,7 +110,46 @@ for i=1:run_in
     
     pause(1)
     pourcentage=[sprintf('%0.2f',(i/run_in)*100) ' %'];
-    waitbar(i/run_in,progress,sprintf('%s',['CALIBRATION PROGRESS :' pourcentage]));
+    waitbar(i/run_in,progress,sprintf('%s',[handles.language.calibration_msg1 ' :' pourcentage]));
+
+end
+delete(progress)
+
+%% GET TIME FOR START %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+timeGPS=QuickSendReceive(handles.CHANNEL.ch_serial{1,1},'gettime',10,'LastTime&DatefromGPSwas:','(');
+time_GPS_dec=datenum(timeGPS,'yyyy-mm-dd,HH:MM:SS');  
+time_GPS_dec=time_GPS_dec+define_process_time/86400;
+start_time=datestr(time_GPS_dec,'yyyy-mm-dd,HH:MM:SS');
+
+%% COLLECT CALLIBRATION 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+timerVal = tic;
+FREQ=[1,2,4,8,16,32];
+DUTY=100;
+SR=[2,2,2,2,2,2];
+gain=[0,0,0,0,0,0];
+cycle=[8,8,16,32,64,128];
+run_for=ceil((1./FREQ).*cycle+(1./FREQ)+6);
+time_between_files=3;
+letters='_MCAL';
+    
+% RUN CALIBRATION ACQUISTION
+[run_in]=l_run_schedule(start_time,false,letters,handles,FREQ,DUTY,SR,gain,run_for,time_between_files,0);
+run_in=run_in+define_process_time-ceil(toc(timerVal))+2;
+
+
+%% WAIT FOR END OF ACQUISITION
+
+    progress = waitbar(0,handles.language.progress_title4,'Name',handles.language.progress_title4 ...
+    ,'Position',[handles.main.GUI.left_bar handles.main.GUI.bottom_bar ...
+    handles.main.GUI.width_bar handles.main.GUI.height_bar]);   
+    set(progress, 'WindowStyle','modal', 'CloseRequestFcn','');
+    
+for i=1:run_in
+    
+    pause(1)
+    pourcentage=[sprintf('%0.2f',(i/run_in)*100) ' %'];
+    waitbar(i/run_in,progress,sprintf('%s',[handles.language.calibration_msg1 ' :' pourcentage]));
 
 end
 delete(progress)
@@ -118,26 +162,27 @@ time_GPS_dec=datenum(timeGPS,'yyyy-mm-dd,HH:MM:SS');
 time_GPS_dec=time_GPS_dec+define_process_time/86400;
 start_time=datestr(time_GPS_dec,'yyyy-mm-dd,HH:MM:SS');
 
-%% COLLECT CALLIBRATION 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% COLLECT CALLIBRATION 3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 timerVal = tic;
-FREQ=[0.125,0.25,0.5,1,2,4,8,16,32,64,128,256,512,1024];
+FREQ=[1,2,4,8,16,32,64,128];
 DUTY=100;
-SR=[4,4,4,4,4,4,4,4,4,4,4,4,4,4];
-gain=[0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-cycle=[4,4,8,8,16,32,64,128,256,512,1024,2048,4096,8192];
+SR=[4,4,4,4,4,4,4,4];
+gain=[0,0,0,0,0,0,0,0];
+cycle=[8,16,32,64,64,128,256,512];
 run_for=ceil((1./FREQ).*cycle+(1./FREQ)+4);
 time_between_files=3;
 letters='_HCAL';
 
     
 % RUN CALIBRATION ACQUISTION
-[run_in]=l_run_schedule(start_time,false,1,letters,handles,FREQ,DUTY,SR,gain,run_for,time_between_files,0);
+[run_in]=l_run_schedule(start_time,false,letters,handles,FREQ,DUTY,SR,gain,run_for,time_between_files,0);
 run_in=run_in+define_process_time-ceil(toc(timerVal))+2;
 
 
 %% WAIT FOR END OF ACQUISITION
 
-    progress = waitbar(0,'CALIBRATION','Name','CALIBRATION' ...
+    progress = waitbar(0,handles.language.progress_title4,'Name',handles.language.progress_title4 ...
     ,'Position',[handles.main.GUI.left_bar handles.main.GUI.bottom_bar ...
     handles.main.GUI.width_bar handles.main.GUI.height_bar]);   
     set(progress, 'WindowStyle','modal', 'CloseRequestFcn','');
@@ -146,7 +191,7 @@ for i=1:run_in
     
     pause(1)
     pourcentage=[sprintf('%0.2f',(i/run_in)*100) ' %'];
-    waitbar(i/run_in,progress,sprintf('%s',['CALIBRATION PROGRESS :' pourcentage]));
+    waitbar(i/run_in,progress,sprintf('%s',[handles.language.calibration_msg1 ' :' pourcentage]));
 
 end
 delete(progress)
@@ -193,7 +238,7 @@ end
 % CHECK THAT ALL SDs APPEARED
 l_SDavailable2( handles,NbD_b_UMASS,COM_Nb );
 
-
+pause(2)
 %% Copy files
         EXT='*CAL.Z3D';
         folder=['calibrate' filesep 'temp'];
@@ -207,13 +252,13 @@ l_SDavailable2( handles,NbD_b_UMASS,COM_Nb );
         
 % Delete file
          delete_files(handles.main.GUI.left_bar,handles.main.GUI.bottom_bar, ...
-                        handles.main.GUI.width_bar,handles.main.GUI.height_bar)   
+                        handles.main.GUI.width_bar,handles.main.GUI.height_bar,'*.Z3D')   
 
 %% DELETE SERIAL OBJ
 newobjs=instrfind;if ~isempty(newobjs);fclose(newobjs);end
 
 %% ANALYSE CALIBRATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%dir_path='C:\for_redistribution_files_only\calibrate\temp\2014-10-01\07_34_marc.benoit_ZEN1\ZenRawData';
+%dir_path='C:\ZenACQ_matlab\calibrate\temp\2014-11-02\14_05_marc.benoit_ZEN1\ZenRawData';
 
 error=false;
 DATA=data_upload(dir_path,handles);
@@ -239,13 +284,43 @@ for sch=1:max_sch
             case 0.25
                 stacks=3;
             case 0.5
+                stacks=3;
+            case 1
+                stacks=6;
+            case 2
+                stacks=6;
+            case 4
+                stacks=6;
+            case 8
+                stacks=16;      
+            case 16
+                stacks=16;   
+            case 32
+                stacks=32;      
+            case 64
+                stacks=32;
+        end
+        
+    elseif ADfreq==1024
+        switch Tx_freq
+            case 1.5625E-2
+                stacks=2;
+            case 3.1250E-2
+                stacks=2;
+            case 6.2500E-2
+                stacks=2;
+            case 0.125
+                stacks=4;
+            case 0.25
+                stacks=3;
+            case 0.5
                 stacks=6;
             case 1
-                stacks=3;
+                stacks=6;
             case 2
-                stacks=8;
+                stacks=6;
             case 4
-                stacks=16;
+                stacks=14;
             case 8
                 stacks=16;      
             case 16
@@ -265,7 +340,7 @@ for sch=1:max_sch
             case 0.5
                 stacks=6;
             case 1
-                stacks=5;
+                stacks=2;
             case 2
                 stacks=8;
             case 4
@@ -309,8 +384,8 @@ min_length=(Tx_period*ADfreq)+(stacks*pts_stack)+offset;
  
 if error==true;
     stacks=stacks-1;
+    continue;
 end
- 
 
 % ARRAY TSs
 clear TS ts_t
@@ -349,7 +424,8 @@ TS=zeros(pts_stack,chn);
   semilogx(f,PHASE)
   subplot(3,1,3)
   semilogx(f,MAG)
-  pause(1)
+  pause(0.8)
+  saveas(figure(5+sch),['calibrate/fig' num2str(5+sch)], 'jpg')
   close(figure(5+sch))
 end
 
@@ -361,26 +437,45 @@ if ~exist('calibrate','dir');mkdir('calibrate');end
 file_name=['calibrate/ZEN' num2str(DATA(1,1).Box_id) '.cal'];
 fid = fopen(file_name,'w');
 
-line1='$CAL.VER,025';
-line2='$CAL.LABEL,CARDSN,APP,DATE,TIME,TEMP,MODE,VOLTS,CALFREQ,ADFREQ,G0,ATTEN,MAG1,PHASE1,MAG3,PHASE3,MAG5,PHASE5,MAG7,PHASE7,MAG9,PHASE9';
-line3='$CAL.HARMONICS,13579';
+
+
+line1='$GDP.TYPE,Zen';
+line2='$GDP.PROGVER,ZenACQ';
+line3=['$GDP.DATE,' datestr(time,'mm/dd/yyyy')];
+line4=['$GDP.TIME,' datestr(time,'HH:MM:SS')];
+line5='$GDP.BAT,12.6';
+line6='$GDP.TEMP,33.25';
+line8='$GDP.SIGSOURCE,ISys';
+line9='$GDP.CALVOLTS,1.000';
+line10='$CAL.VER,025';
+line11='$CAL.LABEL,CARDSNX,HDWKEY,CALFREQ,ADFREQ,G0,ATTEN,MAG1,PHASE1,MAG3,PHASE3,MAG5,PHASE5,MAG7,PHASE7,MAG9,PHASE9';
+line12='$CAL.HARMONICS,13579';
 
 fprintf(fid,'%s\n',line1);
 fprintf(fid,'%s\n',line2);
 fprintf(fid,'%s\n',line3);
+fprintf(fid,'%s\n',line4);
+fprintf(fid,'%s\n',line5);
+fprintf(fid,'%s\n',line6);
+fprintf(fid,'%s\n',line8);
+fprintf(fid,'%s\n',line9);
+fprintf(fid,'%s\n',line10);
+fprintf(fid,'%s\n',line11);
+fprintf(fid,'%s\n',line12);
 
 for freq=1:max_sch
     for chn=1:max_chn
-        fprintf(fid,'%s\t',DATA(freq,chn).Serial);
-        fprintf(fid,'%s\t','ZenACQ');
-        fprintf(fid,'%s\t',datestr(time,'mm/dd/yyyy'));
-        fprintf(fid,'%s\t',datestr(time,'HH:MM:SS'));
-        fprintf(fid,'%s\t','33.25');
-        fprintf(fid,'%s\t','INT');
-        fprintf(fid,'%s\t\t','12.6');
+        fprintf(fid,'%s ',DATA(freq,chn).Serial);
+        if DATA(freq,chn).ADfreq==256
+            fprintf(fid,'%s  ','00000000');
+        elseif DATA(freq,chn).ADfreq==1024
+            fprintf(fid,'%s  ','00000002');
+        elseif DATA(freq,chn).ADfreq==4096
+            fprintf(fid,'%s  ','00000004');
+        end
         fprintf(fid,'%f\t',DATA(freq,chn).period_divider);
         fprintf(fid,'%f\t',DATA(freq,chn).ADfreq);
-        fprintf(fid,'%d\t',DATA(freq,chn).gain);
+        fprintf(fid,'%d ',DATA(freq,chn).gain);
         fprintf(fid,'%d\t',0);
         for harmonic=1:size(DATA(freq,chn).MAG_PHASE.mag,1)
         fprintf(fid,'%f ',DATA(freq,chn).MAG_PHASE.mag(harmonic));
