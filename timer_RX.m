9b008db591d184be7818889b2b6210e023b000c2
k4lls <materre@gmail.com>
2014-11-05 18:07:34 +0100

    v3

--- timer_RX.m
+++ timer_RX.m
@@ -20,7 +20,7 @@ if strcmp('NONE',COM{1,1})
     % closes the figure
     delete(handles.ZenRX);
     catch
-        disp('error when close')
+        disp('Error when close the windows')
     end
     
     % DELETE EXISTING TIMER
@@ -45,19 +45,22 @@ if msg_val>0                                % IF THERE IS A SCHEDULE COMMITED
     dif_str = datestr(dif,'dd-HH:MM:SS');
     if dif<0                                % IF BEGINNING OF THE SCHEDULE
         set(handles.error_msg,'Value',-1);
-        set(handles.error_msg,'String','ZEN is running. Do not unplug');        
+        set(handles.error_msg,'String',handles.language.timer_msg1);        
         
     else                                    % SHOW COUNTDOWN UNTIL BEGINNING OF THE FIRST SCHEDULE
-        set(handles.error_msg,'String',['Starts in ' dif_str]);
+        set(handles.error_msg,'String',[handles.language.timer_msg2 ' ' dif_str]);
     end
 elseif msg_val==-1                          % START COUNTDOWN UNTIL END OF ACQUISITION
     end_time=get(handles.box_str,'Value');
     dif_end=end_time-time;
     if dif_end>0                            % DISPLAY COUNTDOWN
         end_time_str=datestr(dif_end,'dd-HH:MM:SS');
-        set(handles.error_msg,'String',['Finishes in ' end_time_str]);
+        set(handles.error_msg,'String',[handles.language.timer_msg3 ' ' end_time_str]);
     else                                    % END OF COUNTDOWN, STOP DISPLAYING.
         set(handles.error_msg,'String','');
+        if handles.main.type==1 % if TX Disable Transmit
+            set(handles.set_up,'Enable','on')
+        end
     end
 end
 
@@ -91,28 +94,43 @@ for i=1:size(handles.CHANNEL.ch_serial,2)
 end
 
      % IF THE TOTAL SUM EQUAL THE AMOUNT OF CHANNEL DISPLAY SYNC CIRCLE
-     
      if total_Y==size(handles.CHANNEL.ch_serial,2)
          set(handles.status_sync_val,'ForegroundColor','green')
          
          % GET TIME
          if get(handles.local_time_val,'Value')==0
-         set(handles.gps_time_str,'Visible','on')
          set(handles.local_time_val,'Value',1)
          timeGPS=QuickSendReceive(handles.CHANNEL.ch_serial{1,1},'gettime',10,'LastTime&DatefromGPSwas:','(');
          time_GPS_dec=datenum(timeGPS,'yyyy-mm-dd,HH:MM:SS')+time_zone+leak_second;
          dif_time=time_GPS_dec-time;
          GPS_dif_time=datestr(dif_time,'dd, HH:MM:SS');
 
-         if abs(dif_time*86400)>30
-             warndlg(['Your computer time does not match the GPS time. Please verify your Time zone or your computer time settings. Time difference : ' ...
-                 GPS_dif_time ],'!! Time Zone Aberation !!')
+         if abs(dif_time*86400)>120
+             warndlg([handles.language.time_zone_err1 ' ' ...
+                 GPS_dif_time ], handles.language.time_zone_err2)
          end
          end
-         
-         
+     
+     getlla=QuickSendReceive(handles.CHANNEL.ch_serial{1,handles.CH1_index},'GETLLA',10,'LastlocationfromGpswas:','m');  
+     getlla_cell=textscan(getlla,'%fox%fo@%f');
+     Alt=getlla_cell{1,3};
+     [x,y,utmzone] = deg2utm(getlla_cell{1,1},getlla_cell{1,2});   
+     set(handles.utm_checkbox,'Visible','on')
+     set(handles.utm_zone_str,'String',[handles.language.zen_zone_str ' ' utmzone],'Value',round(x*1)/1)
+     set(handles.altitude_str,'String',[handles.language.zen_altitude ' ' num2str(round(Alt)) ' m'],'Value',round(y*1)/1)
+
+         if handles.main.type==1 % TX
+            set(handles.set_up,'Visible','on')
+            set(handles.error_msg,'String','')
+         end
+     
      else
          set(handles.status_sync_val,'ForegroundColor','red')
+         
+         if handles.main.type==1 % TX
+            set(handles.set_up,'Visible','off')
+            set(handles.error_msg,'String',handles.language.transmit_msg)
+         end
      end
 
 end
@@ -126,18 +144,31 @@ if mod(second,7)==0
     set(handles.status_sats_val,'String',numsat);
 
     % VOLTAGE
-    Voltage=QuickSendReceive(handles.CHANNEL.ch_serial{1,handles.CH1_index},'TESTPWRADCS',10,'TestPwrAdcs:','Vx');
-    set(handles.status_volts_val,'String',Voltage);
+    Voltage_str=QuickSendReceive(handles.CHANNEL.ch_serial{1,handles.CH1_index},'TESTPWRADCS',10,'TestPwrAdcs:','Vx');
 
-else
+    set(handles.status_volts_val,'String',Voltage_str);
+    if str2double(Voltage_str)<9
+        set(handles.status_volts_val,'Value',1);  
+    else
+        set(handles.status_volts_val,'Value',0);  
+    end
 
-    % EXTRAPOLATE TIME BASED ON EVERY 7second value.
-    %GPS_time_display=get(handles.gps_time_val,'String');
-    %if ~isempty(GPS_time_display)
-    %    GPS_time_display=datestr(datenum(GPS_time_display,'mmm dd, yyyy HH:MM:SS')+(1/86400),'mmm dd, yyyy HH:MM:SS');  %% + 16second leak  + time_zone
-    %end
+    
 end
 
 %% UPDATE GUI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 time_display=datestr(time,'mmm dd, yyyy HH:MM:SS');
 set(handles.local_time_val,'String',time_display)
+
+voltage_status=get(handles.status_volts_val,'Value');
+if voltage_status==1
+    set(handles.status_volts_val,'BackgroundColor','red');
+    set(handles.status_volts,'BackgroundColor','red');
+    pause(0.1)
+    set(handles.status_volts_val,'BackgroundColor',[0.94 0.94 0.94]);
+    set(handles.status_volts,'BackgroundColor',[0.94 0.94 0.94]);
+else
+    set(handles.status_volts_val,'BackgroundColor',[0.94 0.94 0.94]);
+    set(handles.status_volts,'BackgroundColor',[0.94 0.94 0.94]);
+end
+
