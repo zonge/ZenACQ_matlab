68884d356d727eda6aca93054d780dba253296d9
k4lls <materre@gmail.com>
2014-11-05 18:08:07 +0100

    v3

--- l_sch_set_table.m
+++ l_sch_set_table.m
@@ -8,22 +8,32 @@ for i=1:nb_schedule
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     dec_time=double(handles.SCHEDULE(i,1).duration)/86400;
     
-    [ day_str2, hour_str2, min_str2, sec_str2 ] = l_sch_displayTime( dec_time );
+    if dec_time*86400>1
     
-    data{i,1}=[day_str2 hour_str2 min_str2 sec_str2];
+        [ day_str2, hour_str2, min_str2, sec_str2 ] = l_sch_displayTime( dec_time );
+    
+        data{i,1}=[day_str2 hour_str2 min_str2 sec_str2];
+    
+    else
+        
+        data{i,1}='less than a second';
+        
+    end
   
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% SR  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    SR=handles.SCHEDULE(i,1).sr;
+% TABLE 2  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+
+if handles.main.type==0 % RX
     
+    SR=handles.SCHEDULE(i,1).sr;    
     switch SR
         case 0
             SR_str='Low Band (256Hz)';
         case 1
             SR_str='512 Hz';
         case 2
-            SR_str='1024 Hz';
+            SR_str='Mid Band (1024 Hz)';
         case 3
             SR_str='2048 Hz';
         case 4
@@ -32,10 +42,16 @@ for i=1:nb_schedule
     
     data{i,2}=SR_str;
     
+elseif handles.main.type==1 % TX
+
+    data{i,2}=handles.SCHEDULE(i,1).TX_freq;
+end
+    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% GAIN  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TABLE 3  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    Gain=handles.SCHEDULE(i,1).gain;
+    
+Gain=handles.SCHEDULE(i,1).gain;
     
      switch Gain
         case 1
@@ -56,7 +72,14 @@ for i=1:nb_schedule
             gain_str='Auto';
      end
      
+if handles.main.type==0
     data{i,3}=gain_str;
+elseif handles.main.type==1
+    disp(handles.SCHEDULE(i,1).duration)
+    disp(handles.SCHEDULE(i,1).TX_freq)
+    data{i,3}=handles.SCHEDULE(i,1).duration*handles.SCHEDULE(i,1).TX_freq;  
+end
+
 end
 
 set(handles.schedule_tbl,'Data',data)
