153a8af67e893f4ef13078dd1c16411e35a53a07
k4lls <materre@gmail.com>
2014-11-05 18:07:47 +0100

    v3

--- m_get_setting_key.m
+++ m_get_setting_key.m
@@ -15,9 +15,12 @@ file_content=l_get_file_content( ext,handles,display_msg );
     
     % DEFINE KEY WORDS !
     keys.z3d_location=l_search_key('$z3d_location',file_content);
-    keys.Rx_geometry_selected=l_search_key('$Rx_geometry_selected',file_content,ext);
+    keys.Rx_geometry_selected_MT=l_search_key('$Rx_geometry_selected_MT',file_content,ext);
+    keys.Rx_geometry_selected_IP_RX=l_search_key('$Rx_geometry_selected_IP_RX',file_content,ext);
+    keys.Rx_geometry_selected_IP_TX=l_search_key('$Rx_geometry_selected_IP_TX',file_content,ext);
     keys.Rx_schedule_selected=l_search_key('$Rx_schedule_selected',file_content,ext);
     keys.time_zone=l_search_key('$time_zone',file_content,ext);
+    keys.ZenACQ_mode=l_search_key('$ZenACQ_mode',file_content,ext);
     
     
     
