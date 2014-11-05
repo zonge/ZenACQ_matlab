6c156321a4f0d138ce8c6a686e78e3d669642aff
k4lls <materre@gmail.com>
2014-11-05 18:08:30 +0100

    v3

--- l_find_schedule.m
+++ l_find_schedule.m
@@ -6,7 +6,16 @@ if isempty(handles.SCH)
     return;
 end
 
-file_dir=dir(['schedule' filesep '*.sch']);
+survey_type=str2double(handles.setting.ZenACQ_mode);
+if survey_type==1
+    EXT='MTsch';
+elseif survey_type==2 && handles.main.type==0
+    EXT='IPsch';
+elseif survey_type==2 && handles.main.type==1
+    EXT='TXsch';
+end
+
+file_dir=dir(['schedule' filesep '*.' EXT]);
 if isempty(file_dir)
     set(handles.schedule_popup,'String','No schedule found')
     set(handles.edit_push,'Visible','off')
@@ -14,41 +23,43 @@ else
     
     Files=cell(size(file_dir,1),1);
     if strcmp(handles.SCH.last_schedule,'$Rx_schedule_selected')
-    	
         for i=1:size(file_dir,1)
-            Files{i,1}=file_dir(i).name(1:end-4);
+            Files{i,1}=file_dir(i).name(1:end-6);
         end
         set(handles.edit_push,'Visible','on')
         set(handles.schedule_popup,'String',Files)  
     else
     
         for i=1:size(file_dir,1)
-            File=file_dir(i).name(1:end-4);
+            File=file_dir(i).name(1:end-6);
             if strcmp(File,handles.SCH.last_schedule)
                index=i;
             end
         end
         if exist('index','var')        
-        Files{1,1}=file_dir(index).name(1:end-4);
+        Files{1,1}=file_dir(index).name(1:end-6);
         j=1;
             for i=1:size(file_dir,1)
                 if i~=index
                     j=j+1;
-                    Files{j,1}=file_dir(i).name(1:end-4);
+                    Files{j,1}=file_dir(i).name(1:end-6);
                 end
             end
             set(handles.edit_push,'Visible','on')
             set(handles.schedule_popup,'String',Files)
         else
 
-            set(handles.schedule_popup,'String','No schedule found')
-            set(handles.edit_push,'Visible','off')
+            for i=1:size(file_dir,1)
+                Files{i,1}=file_dir(i).name(1:end-6);
+            end
+            set(handles.edit_push,'Visible','on')
+            set(handles.schedule_popup,'String',Files)  
             
         end
             
     end
     
-    main_file=['schedule' filesep Files{1,1} '.sch'];
+    main_file=['schedule' filesep Files{1,1} '.' EXT];
 
 end
 
