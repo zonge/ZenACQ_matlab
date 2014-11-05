c8b35b1b941d2678e47f2d859866a37a25642314
k4lls <materre@gmail.com>
2014-11-05 18:09:17 +0100

    v3

--- connect2.m
+++ connect2.m
@@ -10,6 +10,9 @@ header = waitln(SERIAL_obj,'Brd339>','Read HEADER',10);
 
 c.ChNb=str2double(GetString(header,'ChannelNumvaluefoundinEEProm:','.'));
 c.BoxNb=str2double(GetString(header,'ZenBoxNumbervaluefoundinEEProm:','.'));
+c.LogData=str2double(GetString(header,'LogDataToFlashvaluefoundinEEProm:','.'));
+c.LogTerminal=str2double(GetString(header,'LogTerminalInDatafilesvaluefoundinEEProm:','.'));
+c.Datatype=str2double(GetString(header,'DatafileTypevaluefoundinEEProm:','.'));
 version1=GetString(header,'Main.hexRevision:',';BuildNum:');
 version2=GetString(header,';BuildNum:',';BuildDate:');
 
