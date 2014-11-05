5419292226c92aaae02801ab0d397725a33ef52d
k4lls <materre@gmail.com>
2014-11-05 18:08:19 +0100

    v3

--- l_run_schedule.m
+++ l_run_schedule.m
@@ -1,9 +1,11 @@
-function [ run_in ] = l_run_schedule(time,cal,mux,letter,handles,FREQ,DUTY,SR,gain,run_for,time_between_files,run_in )
+function [ run_in ] = l_run_schedule(time,cal,letter,handles,FREQ,DUTY,SR,gain,run_for,time_between_files,run_in,tbl_content )
     
+
     ADC_freq=2097152; %ADC speed CONSTANT VARIABLE
 
+    ch_num=get(handles.geometry_table,'RowName');
     
-    progress = waitbar(0,'Set Schedule...','Name','Set Schedule' ...
+    progress = waitbar(0,handles.language.progress_str6,'Name',handles.language.progress_title2 ...
     ,'Position',[handles.main.GUI.left_bar handles.main.GUI.bottom_bar ...
     handles.main.GUI.width_bar handles.main.GUI.height_bar]);    
 
@@ -12,40 +14,28 @@ function [ run_in ] = l_run_schedule(time,cal,mux,letter,handles,FREQ,DUTY,SR,ga
         % Clear schedule
         QuickSendReceive(handles.CHANNEL.ch_serial{ch},'clearschedule',10,'Entirescheduledeleted','T');
         
-        waitbar(1,progress,sprintf('%s','Clear Schedule '));
-    end    
+        waitbar(1,progress,sprintf('%s',handles.language.progress_str6));
+    end 
+    
+    close(progress)
     
     if cal==true
         
-    period=(ADC_freq/FREQ)-1;
-    duty=(DUTY*ADC_freq/100/FREQ)-1;
-    if DUTY==0;duty=0;end
-    if FREQ==0;period=0;end
+        % CAL voltage
+        QuickSendReceive(handles.CHANNEL.ch_serial{handles.CH1_index},'calvoltage 0',10,'Voltageto:',';');
     
-    % CAL voltage
-    QuickSendReceive(handles.CHANNEL.ch_serial{C.index_ch1},'calvoltage 0.5',10,'Voltageto:',';');
-        
-    % CAL ADC period
-    QuickSendReceive(handles.CHANNEL.ch_serial{C.index_ch1},['adcperiod ' num2str(period)],10,'perioddivisorsetto:','.');
+        % CAL channel
+        QuickSendReceive(handles.CHANNEL.ch_serial{handles.CH1_index},'calchannels 0x00',10,'maskiscurrentlysetto:0x','.');
 
-    % CAL ADC Duty
-    QuickSendReceive(handles.CHANNEL.ch_serial{C.index_ch1},['adcduty ' num2str(duty)],10,'dutydivisorsetto:','.');
-
-    % CAL channel
-    QuickSendReceive(handles.CHANNEL.ch_serial{C.index_ch1},'calchannels 0x00',10,'maskiscurrentlysetto:0x','.');
-
+    
     end
         
-    for ch=1:size(handles.CHANNEL.ch_serial,2)
-         
-        % Set Mux
-        QuickSendReceive(handles.CHANNEL.ch_serial{ch},['adcmux ' num2str(mux)],10,'MuxRegisteriscurrently:0x',0);  
-        
-    end 
-    
-    
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+    progress = waitbar(0,handles.language.progress_str4,'Name',handles.language.progress_title2 ...
+    ,'Position',[handles.main.GUI.left_bar handles.main.GUI.bottom_bar ...
+    handles.main.GUI.width_bar handles.main.GUI.height_bar]);
+    
     % Send Schedule command
     max_schedule=size(SR,2); 
     for schedule=1:max_schedule
@@ -57,13 +47,17 @@ function [ run_in ] = l_run_schedule(time,cal,mux,letter,handles,FREQ,DUTY,SR,ga
         
         
         for ch=1:size(handles.CHANNEL.ch_serial,2)
+            if nargin>11
+            index=strfind(ch_num',num2str(handles.CHANNEL.ch_info{1,ch}.ChNb));
+            if strcmp(tbl_content{index,1},'Off'); continue;end
+            end
+            
             file_name=[sprintf('%.2d',schedule) letter '.z3d'];
             l_run_schedule_low(handles.CHANNEL.ch_serial{ch},file_name,time,run_in,run_for(schedule),SR(schedule),gain(schedule),period,duty);
         end
         
         pourcentage=[sprintf('%0.2f',(schedule/max_schedule)*100) ' %'];
-        waitbar(schedule/max_schedule,progress,sprintf('%s',['Set schedule :' pourcentage]));
-        
+        waitbar(schedule/max_schedule,progress,sprintf('%s',[handles.language.progress_str4 ' ' pourcentage]));
         run_in=run_in+time_between_files+run_for(schedule);
     end
     
